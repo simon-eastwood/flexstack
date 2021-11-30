e824ac87968b1ec01f79a65ef89f99a3c95891f3
Simon Eastwood <seastwood@epo.org>
2021-11-30 19:47:31 +0100

    save a copy

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import 'flexlayout-react/style/light.css'
 
 import { Layout, Model, TabNode, TabSetNode, IJsonModel, Action, Actions, Node as FLNode } from 'flexlayout-react';
 
-import { analyseModel, stackZAxis, IAnalyzedModel } from './FlexModelUtils';
+import { analyseModel, stackZAxis, IAnalyzedModel, stackYAxis } from './FlexModelUtils';
 
 import useMedia from './hooks/useMediaQuery';
 import useWindowSize from './hooks/useWindowSize';
@@ -21,14 +21,17 @@ var json: IJsonModel = {
         "type": "tabset",
         "weight": 50,
         "selected": 0,
-        "minWidth": 200,
-        "minHeight": 200,
+        "minWidth": 510,
+        "minHeight": 50,
         "children": [
           {
             "type": "tab",
             "name": "Things to try",
             "component": "text",
-            "config": { "text": "<ul><li>drag tabs</li><li>drag splitters</li><li>double click on tab to rename</li><li>double click on tabstrip to maximize</li><li>use the Add button to add another tab</li></ul>" }
+            "config": {
+              "text": "<ul><li>drag tabs</li><li>drag splitters</li><li>double click on tab to rename</li><li>double click on tabstrip to maximize</li><li>use the Add button to add another tab</li></ul>"
+
+            }
           }
         ]
       },
@@ -39,8 +42,8 @@ var json: IJsonModel = {
         "type": "tabset",
         "weight": 50,
         "selected": 0,
-        "minWidth": 200,
-        "minHeight": 200,
+        "minWidth": 510,
+        "minHeight": 50,
         "children": [
           {
             "type": "tab",
@@ -54,8 +57,8 @@ var json: IJsonModel = {
         "type": "tabset",
         "weight": 50,
         "selected": 0,
-        "minWidth": 200,
-        "minHeight": 200,
+        "minWidth": 510,
+        "minHeight": 50,
 
         "children": [
           {
@@ -72,25 +75,32 @@ var json: IJsonModel = {
   }
 };
 
+const templateModel = analyseModel(Model.fromJson(json), "full");
 
 
 function App() {
   // currentMOdel is what we're currently rendering.
   // If we need to alter the layout due to size restrictions, the previous state is saved in "fullModel" so that it can be restored later
-  const [currentModel, setCurrentModel] = useState(() => { return analyseModel(Model.fromJson(json)) });
+  const [currentModel, setCurrentModel] = useState(() => { return templateModel });
   const [fullModel, setFullModel] = useState(() => { return currentModel });
-  const [canvas, setCanvas] = useState({ absoluteCanvasHeight: false, absoluteCanvasWidth: false });
+
+  const [canvasToggleAbs, setCanvasToggleAbs] = useState(false);
+  const [stackStrategy, setStackStrategy] = useState('Z');
 
   const containerRef = useRef(null);
   const layoutRef = useRef(null);
 
-  // When crossing breakpoint, 
-  const isTooSmall = useMedia('(max-width: 600px)');
+
+
+  // If too narrow for full model, save it and make an adapted version, 
+  const isTooNarrowForFullModel = useMedia(`(max-width: ${fullModel.widthNeeded}px)`);
   useEffect(() => {
-    if (isTooSmall) {
-      if (currentModel) {
-        console.log("resizing model...")
-        // analyse current model
+    if (currentModel) {
+      if (!isTooNarrowForFullModel) {
+        console.log("restoring model...")
+        setCurrentModel(fullModel);
+      } else {
+        // analyse current model to prepare change
         let analysedModel = analyseModel(currentModel.model);
 
         // save this model
@@ -100,18 +110,33 @@ function App() {
         setFullModel(copyOfCurrent);
 
         // alter current model
-        let newModel = stackZAxis(analysedModel);
+        let newModel: IAnalyzedModel;
 
-        setCurrentModel(newModel)
+        if (isTooNarrowForFullModel) {
+          if (stackStrategy === 'Z') {
+            newModel = stackZAxis(analysedModel);
+            setCanvasToggleAbs(false);
+          } else {
+            newModel = stackYAxis(analysedModel);
+          }
+          setCurrentModel(newModel);
+        }
       }
-    } else {
-      console.log("restoring model...")
-
-      setCurrentModel(fullModel)
     }
+  }, [isTooNarrowForFullModel]);
 
-  }, [isTooSmall]);
 
+  // If too short for current model switch to absolute, 
+  const isTooShortForCurrentModel = useMedia(`(max-height: ${currentModel.heightNeeded}px)`);
+  useEffect(() => {
+    if (currentModel) {
+      if (!isTooShortForCurrentModel) {
+        setCanvasToggleAbs(false); console.log("REL CANVAS :" + currentModel.heightNeeded);
+      } else {
+        setCanvasToggleAbs(true); console.log("ABS CANVAS :" + currentModel.heightNeeded);
+      }
+    }
+  }, [isTooShortForCurrentModel]);
 
 
   const factory = (node: TabNode) => {
@@ -131,18 +156,50 @@ function App() {
 
   const interceptAction = (action: Action) => {
     console.log(action);
+
+    if (action.type === Actions.MOVE_NODE) {
+      const fromNode = currentModel.model.getNodeById(action.data.fromNode);
+      const toNode = currentModel.model.getNodeById(action.data.toNode);
+
+      if ((fromNode && fromNode.getType() === TabNode.TYPE) &&
+        (fromNode.getParent() && fromNode.getParent()!.getType() === TabSetNode.TYPE) &&
+        (toNode && toNode.getType() === TabSetNode.TYPE)) {
+
+        console.log("FROM width " + (fromNode.getParent()! as TabSetNode).getMinWidth());
+        console.log("TO WIDTH = " + (toNode as TabSetNode).getMinWidth());
+      } else {
+        console.log("WIDZH LOST?")
+      }
+    }
     return action;
+  }
+
+  const changeStrategy = (event: any) => {
+    setStackStrategy(event.target.value);
+  }
+
+  const modelChanged = (model: Model) => {
+    console.log("model changed");
+    console.log(model);
+
+    // setCurrentModel(analyseModel(model));
   }
 
 
   return (
 
-    <div className="outer" >
+    <div className="outer" style={canvasToggleAbs ? { height: currentModel.heightNeeded + 'px', width: currentModel.widthNeeded + 'px' } : { height: '100%', width: '100%' }}>
+      <span> Stacking strategy:</span>
+      <select value={stackStrategy} onChange={changeStrategy}>
+        <option value="Z">Z axis</option>
+        <option value="Y">Y axis</option>
+      </select>
       <button onClick={onAdd}>Add</button>
-      <div className="inner" ref={containerRef}>
+      <div className="inner" >
         {currentModel && (
           <Layout ref={layoutRef}
             onAction={interceptAction}
+            onModelChange={modelChanged}
             model={currentModel.model}
             factory={factory} />)}
       </div>
--- src/FlexModelUtils.ts
+++ src/FlexModelUtils.ts
@@ -1,56 +1,139 @@
-import { Model, TabNode, TabSetNode, IJsonModel, Action, Actions, Node as FLNode, DockLocation } from 'flexlayout-react';
+import { Model, TabNode, TabSetNode, IJsonModel, Action, Actions, Node as FLNode, DockLocation, RowNode } from 'flexlayout-react';
 
 
 
 export interface IAnalyzedModel {
+    name?: string,
     model: Model,
     activeTabset: FLNode | undefined,
-    firstTabset: FLNode | undefined,
+    prioTabset: FLNode | undefined,
     widthNeeded?: number,
-    heightNeeded?: number
+    heightNeeded?: number,
+    rootRow?: FLNode | undefined
 }
 
-export const analyseModel = (modelToAnalyse: Model): IAnalyzedModel => {
+export const analyseModel = (modelToAnalyse: Model, name?: string): IAnalyzedModel => {
 
-    let firstTabset: TabSetNode | undefined = undefined;
+    let prioTabset: TabSetNode | undefined = undefined;
     let activeTabset: TabSetNode | undefined = undefined;
+    let row: RowNode | undefined = undefined;
+    let widthNeeded = 0;
+    let heightNeeded = 0;
+
 
     console.log("doign analysis");
     // find the tabset that is currently active, and also the first tabset (as fallback)
     modelToAnalyse.visitNodes(node => {
-        if (!firstTabset && node.getType().toLowerCase() === 'tabset') firstTabset = node as TabSetNode;
+        console.log(node.getType() + " is " + node.getId());
+        if (node.getChildren()) {
+            console.log("has children");
+            node.getChildren().forEach(node => console.log("  " + node.getId() + ","))
+        } else console.log("has NO children");
+
+        if (node.getParent()) {
+            console.log("has parent " + node.getParent()!.getId());
+        } else console.log("has NO parent");
+
+
+
+        if (!prioTabset && node.getType().toLowerCase() === 'tabset') prioTabset = node as TabSetNode;
 
         if (!activeTabset && node.getType().toLowerCase() === 'tabset' && (node as TabSetNode).isActive()) {
             activeTabset = node as TabSetNode;
         }
+
+        if (!row && node.getType().toLowerCase() === 'row' && !node.getParent()) {
+            row = node as RowNode;
+        }
+
+        if (node.getType().toLowerCase() === 'tabset') {
+            if ((node as TabSetNode).getMinWidth() > 0) {
+                widthNeeded += (node as TabSetNode).getMinWidth();
+
+            }
+            if ((node as TabSetNode).getMinHeight() > 0) {
+                heightNeeded += (node as TabSetNode).getMinHeight();
+
+            }
+        }
     });
+    console.log("done");
 
-
-    return {
+    const result: IAnalyzedModel = {
+        name: name,
         model: modelToAnalyse,
         activeTabset: activeTabset,
-        firstTabset: firstTabset
+        prioTabset: prioTabset,
+        rootRow: row
     }
+
+    console.log(modelToAnalyse.toJson());
+    if (widthNeeded > 0) result.widthNeeded = widthNeeded; console.log("min width = "); console.log(widthNeeded);
+    if (heightNeeded > 0) result.heightNeeded = heightNeeded; console.log("min height = "); console.log(heightNeeded);
+
+    return result;
 }
 
 
 
 export const stackZAxis = (analyzedModel: IAnalyzedModel): IAnalyzedModel => {
 
-    let targetTabset: FLNode = analyzedModel.activeTabset ? analyzedModel.activeTabset : analyzedModel.firstTabset!;
+    let targetTabset: FLNode = analyzedModel.activeTabset ? analyzedModel.activeTabset : analyzedModel.prioTabset!;
     let tabsToMove: FLNode[] = [];
 
     analyzedModel.model.visitNodes((node) => {
+        console.log(node.getType() + " is " + node.getId());
+        if (node.getParent()) {
+            console.log(" and parent is: " + node!.getParent()!.getId());
+
+        } else {
+            console.log("none")
+        }
         if (node.getType().toLowerCase() === 'tab' && node.getParent()?.getId() != targetTabset!.getId()) {
             tabsToMove.push(node);
         }
     });
 
     tabsToMove.forEach(node => {
-        let mv = Actions.moveNode(node.getId(), targetTabset?.getId(), DockLocation.CENTER, -1);
+        let mv = Actions.moveNode(node.getId(), targetTabset?.getId(), DockLocation.CENTER, -1, false);
         analyzedModel.model.doAction(mv);
     })
 
+
+    return analyseModel(analyzedModel.model);
+}
+
+export const stackYAxis = (analyzedModel: IAnalyzedModel): IAnalyzedModel => {
+    let activeTabset: FLNode = analyzedModel.activeTabset ? analyzedModel.activeTabset : analyzedModel.prioTabset!;
+    let targetRow = analyzedModel.rootRow;
+    let tabSetsToMove: FLNode[] = [];
+
+    if (targetRow) {
+
+        analyzedModel.model.visitNodes((node) => {
+            console.log(node.getType() + " is " + node.getId());
+            if (node.getType().toLowerCase() === 'tabset' && node.getId() != activeTabset.getId()) {
+                tabSetsToMove.push(node);
+            }
+        });
+
+
+        tabSetsToMove.forEach(node => {
+            let mv = Actions.moveNode(node.getId(), targetRow!.getId(), DockLocation.BOTTOM, -1);
+            console.log(mv);
+
+            console.log("before parent IS " + node.getParent()!.getId())
+            console.log("node is "); console.log(node.toJson());
+            analyzedModel.model.doAction(mv);
+
+            // targetRow = node.getParent();
+            console.log("after parent IS " + node.getParent()!.getId())
+
+
+
+        })
+
+    }
 
     return analyseModel(analyzedModel.model);
 }
